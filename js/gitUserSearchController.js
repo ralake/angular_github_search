c362c50cbb0f67f75e0b7c6e3d3cf62d3f812d3b
Richard Lake <ralake1985@gmail.com>
2014-12-04 13:46:12 +0000

    Ensures searches are not made when the search field is empty

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -3,9 +3,12 @@ gitHubUserSearch.controller('GitUserSearchController', function($scope, $resourc
 var searchResource = $resource('https://api.github.com/search/users');
 
   $scope.doSearch = function() {
+    if($scope.searchTerm !== ""){
     $scope.searchResult = searchResource.get({
-      q: $scope.searchTerm
+      q: $scope.searchTerm,
+      access_token: "8a57f89da099e97a3273104ea0c833c428833dc6"
     });
+    }
   };
 
 });
