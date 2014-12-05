941cb950dcc1b3bcb3147730dd8d8e0cb8c51210
Richard Lake <ralake1985@gmail.com>
2014-12-05 09:02:06 +0000

    Add extra user info to display

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,6 +1,7 @@
 gitHubUserSearch.controller('GitUserSearchController', function($scope, $resource) {
 
 var searchResource = $resource('https://api.github.com/search/users');
+
 $scope.searchHistory = []
 
   $scope.doSearch = function() {
@@ -19,5 +20,12 @@ $scope.searchHistory = []
   $scope.displayLast = function(searchResults) {
     return searchResults[searchResults.length - 1]
   }
+
+  $scope.showUser = function(username) {
+    var userResource = $resource('https://api.github.com/users/' + username + '/repos');
+    $scope.userRepoResult = userResource.query({
+    access_token: "5d6ffa56fba974adfd2dc6b86157d542c67256c0"
+    })
+  };
 
 });
